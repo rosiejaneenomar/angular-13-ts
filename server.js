1333e2b3b59b6d79c747f1bdf3562ecc64c2b97a
Rosie Jane Enomar <rosie.jane.r.enomar@accenture.com>
2022-04-01 16:49:26 +0800

    update

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<app-name>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<angular-13-ts>/index.html'));});
+'/dist/angular-13-ts/index.html'));});
 app.listen(process.env.PORT || 8080);
